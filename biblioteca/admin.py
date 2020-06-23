86bfaeaed49d4a7dece1834dfda546500538f2f0
Thiago <thiagonicomb.xd@gmail.com>
2020-06-23 17:28:27 -0300

    Modificar admin para editar la interfaz

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -4,9 +4,38 @@ from biblioteca.models import Libro
 from biblioteca.models import Usuario
 from biblioteca.models import Ejemplar
 
+
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ['Titulo', 'Editorial']
+    fields = ['Titulo', 'Editorial']
+
+class LibroInline(admin.TabularInline):
+    model = Libro
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['Nombre', 'Direccion']
+    list_display_links = ['Nombre', ]
+    fieldsets = ( 
+        ('Datos', {
+            'fields': ('Nombre',)
+        }),
+        ('Contacto',{
+            'fields': ('Telefono','Direccion')
+        }),
+    )
+
+class AutorAdmin(admin.ModelAdmin):
+    inlines = [LibroInline,]
+    search_fields = ['Nombre']
+
+class EjemplarAutor(admin.ModelAdmin):
+    list_display = ['Libro', 'Usuario']
+    list_filter = ('Libro',)
+
+
 # Register your models here.
 
-admin.site.register(Autor, )
-admin.site.register(Libro, )
-admin.site.register(Usuario, )
-admin.site.register(Ejemplar, )
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libro, LibroAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Ejemplar, EjemplarAutor)
